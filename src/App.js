a1513a0195123edef9f29a849f826de738ce94ad
ooioiooGT <gilber1002@byui.edu>
2023-03-22 01:27:19 -0600

    finish adbout page '

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import About from './page/about';
+import About from './page/About';
+import Home from './page/Home';
 
 function App() {
   return (
     <div className="App">
      BYUI Hackathon
+     <Home />
      <About />
     </div>
   );
