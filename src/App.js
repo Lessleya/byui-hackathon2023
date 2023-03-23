5a458f6f3a3b65df0d85b7352978c432a6f1d162
narah10 <kwa18001@byui.edu>
2023-03-22 19:53:25 -0600

    Just did the header but the style cannot be removed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/header";
+import Navbar from "./components/navbar";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-     BYUI Hackathon
+      <Navbar />
+      <Header />
     </div>
   );
 }
